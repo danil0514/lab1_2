6188f826c49ef3e9d793de9b8b7ecf8402662610
stilion <sparta0514@gmail.com>
2022-12-05 19:52:36 +0300

    Update lab1_2.cpp

--- lab1_2/lab1_2.cpp
+++ lab1_2/lab1_2.cpp
@@ -26,7 +26,7 @@ void sumEvenlyOrOdd(string& number, int &sumEvenly, int &sumOdd) {
     for (i = 0; i < number.length(); i++)
     {
         int n = number[number.length()-i-1] - '0';
-        if ((number.length() - i) % 2 == 0)
+        if ((i) % 2 == 0)
         {
             sumEvenly = sumEvenly + n;
         }
